8f24ca29291c97a0cb7be89fc9ab3fc2faf9b05a
Ekim Coşkun <108937192+ekimcoskun@users.noreply.github.com>
2022-08-27 14:47:56 +0300

    Update Note App and add some features

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,4 +1,4 @@
-const notes = getSavedNotes()
+let notes = getSavedNotes()
 
 const filters = {
     searchText: ''
@@ -26,3 +26,23 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
 document.querySelector('#filter-by').addEventListener('change', function (e) {
     console.log(e.target.value)
 })
+
+window.addEventListener('storage', function(e){
+    if(e.key === 'notes'){
+        notes = JSON.parse(e.newValue)
+        renderNotes(notes,filters)
+    }
+})
+
+const birthday = new Date('August 13 1998 13:58:09')
+const watDate = new Date('June 20 1995 20:50')
+
+const timestamp1 = birthday.getTime()
+const timestamp2 = watDate.getTime()
+
+if(timestamp1-timestamp2 <= 0 ){
+    console.log(`Birthday is earlier :  ${birthday.toString()}`)
+}
+else{
+    console.log(`Wat is earlier :  ${watDate.toString()}`)
+}
--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,6 +1,6 @@
 const noteId = location.hash.substring(1)
-const notes = getSavedNotes()
-const note = notes.find(function(note){
+let notes = getSavedNotes()
+let note = notes.find(function(note){
     return note.id === noteId
 })
 
@@ -24,4 +24,19 @@ document.querySelector('#remove-note').addEventListener('click',function(){
     removeNote(noteId)
     saveNotes(notes)
     location.assign("/notes-app/index.html")
+})
+
+window.addEventListener('storage', function(e){
+    if(e.key === 'notes'){
+        notes = JSON.parse(e.newValue)
+        note = notes.find(function(note){
+            return note.id === noteId
+        })
+        
+        if(note === undefined){
+            location.assign("/notes-app/index.html")
+        }
+        document.querySelector('#note-id').value = note.title
+        document.querySelector('#note-body').value = note.body
+    }
 })
