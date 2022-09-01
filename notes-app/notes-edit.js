b0c478973de022c0266cf820fe240ae6188239e2
Ekim Coşkun <108937192+ekimcoskun@users.noreply.github.com>
2022-09-01 23:57:24 +0300

    arrow function conf

--- notes-app/notes-edit.js
+++ notes-app/notes-edit.js
@@ -1,9 +1,7 @@
 const noteId = location.hash.substring(1)
 const dateElement = document.querySelector('#last-edited')
 let notes = getSavedNotes()
-let note = notes.find(function(note){
-    return note.id === noteId
-})
+let note = notes.find((note) => note.id === noteId)
 
 if(note === undefined){
     location.assign("/notes-app/index.html")
@@ -13,31 +11,29 @@ document.querySelector('#note-id').value = note.title
 document.querySelector('#note-body').value = note.body
 dateElement.textContent = generateLastEdited(note.updatedAt)
 
-document.querySelector('#note-id').addEventListener('change',function(e){
+document.querySelector('#note-id').addEventListener('change',(e) => {
     note.title = e.target.value
     note.updatedAt = moment().valueOf()
     dateElement.textContent = generateLastEdited(note.updatedAt)
     saveNotes(notes)
 })
-document.querySelector('#note-body').addEventListener('change',function(e){
+document.querySelector('#note-body').addEventListener('change',(e) => {
     note.body = e.target.value
     note.updatedAt =moment().valueOf()
     dateElement.textContent = generateLastEdited(note.updatedAt)
     saveNotes(notes)
 })
 
-document.querySelector('#remove-note').addEventListener('click',function(){
+document.querySelector('#remove-note').addEventListener('click',() => {
     removeNote(noteId)
     saveNotes(notes)
     location.assign("/notes-app/index.html")
 })
 
-window.addEventListener('storage', function(e){
+window.addEventListener('storage', (e) => {
     if(e.key === 'notes'){
         notes = JSON.parse(e.newValue)
-        note = notes.find(function(note){
-            return note.id === noteId
-        })
+        note = notes.find((note) => note.id === noteId)
         
         if(note === undefined){
             location.assign("/notes-app/index.html")
